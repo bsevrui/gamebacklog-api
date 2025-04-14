e28ec4ce47b421087879d42c4e9870861bf1c7c1
Borja Sevilla <bsevrui@gmail.com>
2025-04-14 14:32:11 +0200

    feat(config): added config file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    date_default_timezone_set('Europe/Madrid');
+
+    define('DB_NAME', '');
+    define('DB_USER', '');
+    define('DB_PASSWORD', '');
+    define('DB_HOST', 'localhost');
+?>
