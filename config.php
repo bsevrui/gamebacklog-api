e6d1920b980860db80c3efca6a68cf7a7827f9e5
Borja Sevilla <bsevrui@gmail.com>
2025-04-16 12:40:30 +0200

    feat(config): added credentials

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
     date_default_timezone_set('Europe/Madrid');
 
-    define('DB_NAME', '');
-    define('DB_USER', '');
-    define('DB_PASSWORD', '');
+    define('DB_NAME', 'gamebacklog');
+    define('DB_USER', 'appuser');
+    define('DB_PASSWORD', 'appUser4cess#');
     define('DB_HOST', 'localhost');
 ?>
