d40900fda99d03bf44dbc7cf379c943c85efe689
Borja Sevilla <bsevrui@gmail.com>
2025-05-20 17:27:44 +0200

    feat(CORS): added CORS config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // CORS
+  app.enableCors({
+    origin: 'http://localhost:8100',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
