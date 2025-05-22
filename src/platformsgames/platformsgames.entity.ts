269cdf594b9a6c2dabe64d16a90a6af97f2d15ed
Borja Sevilla <bsevrui@gmail.com>
2025-05-22 17:52:42 +0200

    feat(platforms-games): added middle entity

---
+++ src/platformsgames/platformsgames.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, Column, ManyToOne, PrimaryColumn, JoinColumn } from "typeorm";
+import { Platform } from "src/platforms/platform.entity";
+import { Game } from "src/games/game.entity";
+
+@Entity({name: 'platformsgames', synchronize: false})
+export class PlatformsGames {
+    @PrimaryColumn()
+    platformId: number;
+    @PrimaryColumn()
+    gameId: number;
+    @Column({type: 'date'})
+    release_date: Date;
+
+    @ManyToOne(() => Platform, platform => platform.games, { onDelete: 'CASCADE' })
+    @JoinColumn({name: 'platformId'})
+    platform: Platform;
+
+    @ManyToOne(() => Game, game => game.platforms, { onDelete: 'CASCADE' })
+    @JoinColumn({name: 'gameId'})
+    game: Game;
+}
