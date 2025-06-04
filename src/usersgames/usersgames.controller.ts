eb9ce1e7fdcfcfbf5d140850992b8adf636b9938
Borja Sevilla <bsevrui@gmail.com>
2025-06-04 16:12:43 +0200

    feat(usergames): added AuthGuards

--- src/usersgames/usersgames.controller.ts
+++ src/usersgames/usersgames.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Param, ParseIntPipe, Patch, Body, Delete, Get } from '@nestjs/common';
+import { Controller, Param, ParseIntPipe, Patch, Body, Delete, Get, UseGuards } from '@nestjs/common';
 import { UsersgamesService } from './usersgames.service';
 import { UpdateUsersGamesDto } from './dto/update-usersgames.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('usersgames')
 export class UsersgamesController {
     constructor(private usersgamesService: UsersgamesService) {}
 
     @Get(':userId/:gameId')
+    @UseGuards(AuthGuard('jwt'))
     async getOne(
         @Param('userId', ParseIntPipe) userId: number,
         @Param('gameId', ParseIntPipe) gameId: number
@@ -15,6 +17,7 @@ export class UsersgamesController {
     }
 
     @Patch(':userId/:gameId')
+    @UseGuards(AuthGuard('jwt'))
     async update(
         @Param('userId', ParseIntPipe) userId: number,
         @Param('gameId', ParseIntPipe) gameId: number,
@@ -24,6 +27,7 @@ export class UsersgamesController {
     }
 
     @Delete(':userId/:gameId')
+    @UseGuards(AuthGuard('jwt'))
     async delete(
         @Param('userId', ParseIntPipe) userId: number,
         @Param('gameId', ParseIntPipe) gameId: number
